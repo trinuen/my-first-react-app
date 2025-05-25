239fe76edaf66fdbc005a15b77bbae832b1251b0
Tri Nguyen <tous0905@gmail.com>
2025-05-25 12:31:42 -0400

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import React, { useState } from "react";
+import Search from "./components/Search.jsx";
+
+const App = () => {
+  const [searchTerm, setSearchTerm] = useState("");
+
+  return (
+    <main>
+      <div className="pattern" />
+
+      <div className="wrapper">
+        <header>
+          <img src="./hero-img.png" alt="Hero Banner" />
+          <h1>
+            Find <span className="text-gradient">Movies</span> You Enjoy Without
+            The Hassle
+          </h1>
+        </header>
+        <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
+        <h1 className="text-white">{searchTerm}</h1>
+      </div>
+    </main>
+  );
+};
+
+export default App;
---
+++ src/starting-examples.jsx
@@ -0,0 +1,39 @@
+import { useState, useEffect } from "react";
+import reactLogo from "./assets/react.svg";
+import "./App.css";
+
+const Card = ({ title }) => {
+  const [count, setCount] = useState(0);
+  const [hasLiked, setHasLiked] = useState(false);
+
+  useEffect(() => {
+    console.log(`${title} has been liked: ${hasLiked}`);
+  }, [hasLiked]);
+
+  useEffect(() => {
+    console.log("CARD RENDERED");
+  }, []);
+
+  return (
+    <div className="card" onClick={() => setCount(count + 1)}>
+      <h2>
+        {title} <br /> {count || null}
+      </h2>
+
+      <button onClick={() => setHasLiked(!hasLiked)}>
+        {" "}
+        {hasLiked ? "❤️" : "🤍"}
+      </button>
+    </div>
+  );
+};
+
+const App = () => {
+  return (
+    <div className="card-container">
+      <Card title="Star Wars" />
+      <Card title="Avatar" />
+      <Card title="The Lion King" />
+    </div>
+  );
+};
